6880c3c162a7d416b923e99bf7c3a8664292ac60
MissOleM20 <oletgl@gmail.com>
2022-07-28 12:20:15 -0700

    liabraries

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,18 @@
+
+/**
+ * _islower - checks lowercase
+ * Return: 1 if c is lowercase
+ * @c: c is a character
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
